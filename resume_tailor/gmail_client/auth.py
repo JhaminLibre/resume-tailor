7ec4dada32c0200d8ea19b8a420c7959658d0359
Matthew Pennisi <matt.pennisi@gmail.com>
2026-08-17 13:52:52 -0700

    fix: add redirect_uri parameter for Google OAuth (even if not using callback)

--- resume_tailor/gmail_client/auth.py
+++ resume_tailor/gmail_client/auth.py
@@ -27,6 +27,7 @@ def get_gmail_service():
         flow = InstalledAppFlow.from_client_secrets_file(
             GMAIL_CREDENTIALS_PATH,
             SCOPES,
+            redirect_uri="http://localhost:8080/",
         )
         auth_url, _ = flow.authorization_url(prompt='consent')
         print("\n🔐 Google OAuth Authorization Required")
